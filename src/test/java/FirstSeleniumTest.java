db0d8b1ded44d562bded83513bd09395f213ce0f
Peter Hudoba <hudi1989@gmail.com>
2021-04-06 17:37:15 +0200

    move to By variables

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,55 +1,60 @@
-package tests;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.*;
 
-public class FirstSeleniumTest {
-    public WebDriver driver;
-    public WebDriverWait wait;
-    
+
+public class FirstSelenium {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
     @Before
     public void setup() {
         WebDriverManager.chromedriver().setup();
-
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         wait = new WebDriverWait(driver, 10);
     }
-    
-    private WebElement waitAndReturnElement(By locator) {
+
+    private final By bodyLocator = By.tagName("body");
+    private final By searchTogglerLocator = By.className("search-bar-toggler");
+    private final By searchLocator = By.xpath("//div[@class='sliding-search-wrapper']/form/input[@name='search']");
+
+
+
+    private WebElement waitVisibiiltyAndFindElement(By locator) {
         this.wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
         return this.driver.findElement(locator);
-    }    
-    
-    @Test
-    public void testSearch() {
-        this.driver.get("https://www.inf.elte.hu/en/");
-       
-        Assert.assertTrue(this.waitAndReturnElement(By.className("footer-block")).getText().contains("2021 ELTE Faculty of Informatics"));
-        
-        this.waitAndReturnElement(By.className("search-bar-toggler")).click();
-        
-        this.waitAndReturnElement(By.name("search")).sendKeys("Students\n");
-
-        WebElement bodyElemnet = this.waitAndReturnElement(By.tagName("body"));
-        System.out.println("-------------------------------------");
-        System.out.println(bodyElemnet.getText());
-        Assert.assertTrue(bodyElemnet.getText().contains("found"));
-        Assert.assertTrue(bodyElemnet.getText().contains("For Students"));
     }
-    
 
+    @Test
+    public void multiplicationTest() {
+        this.driver.get("https://www.inf.elte.hu/en/");
+
+        WebElement resultElement = waitVisibiiltyAndFindElement(bodyLocator );
+        System.out.println(resultElement.getText());
+        Assert.assertTrue(resultElement.getText().contains("2021 ELTE Faculty of Informatics"));
+        
+        
+        WebElement searchTogglerElement = waitVisibiiltyAndFindElement(searchTogglerLocator);
+        searchTogglerElement.click();
+        
+        WebElement searchBarElement = waitVisibiiltyAndFindElement(searchLocator);
+        searchBarElement.sendKeys("Student \n");
+        
+        WebElement bodyElement = waitVisibiiltyAndFindElement(bodyLocator);
+        System.out.println(bodyElement.getText());
+        Assert.assertTrue(bodyElement.getText().contains("Student Support Centre"));
+        
+    }
     
     @After
     public void close() {
